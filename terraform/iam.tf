47eb8237805df7132effd053618faa340c96677f
nagarajucsd <dramarajudramaraju59@gmail.com>
2026-08-13 14:24:27 +0000

    Monitoring : configure cloudwatch agent

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -41,6 +41,12 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
 
   name = "enterprise-devops-instance-profile"
