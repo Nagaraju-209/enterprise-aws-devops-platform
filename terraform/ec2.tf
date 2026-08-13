60785cf804ac13d677e4df3033fc04adfcf0ab62
nagarajucsd <dramarajudramaraju59@gmail.com>
2026-08-13 13:29:35 +0000

    Monitoring : enable detailed EC2 monitoring

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -25,6 +25,8 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
+  monitoring = true
+
   subnet_id = aws_subnet.public.id
 
   vpc_security_group_ids = [
